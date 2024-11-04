7a7f4772a6472d1b6169a28748e12c94781dc39d
Martinbroman90 <Martinbroman90@gmail.com>
2024-11-04 15:40:45 +0100

    Create learn-string-manipulation-by-building-a-cipher.py
    kod från uppgifter 1-36

---
+++ python/learn-string-manipulation-by-building-a-cipher.py
@@ -0,0 +1,10 @@
+
+#step 36
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
